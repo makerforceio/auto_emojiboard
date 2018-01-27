59a8aa8c23800a86caa4555eb54630e658e38c4d
Owen Leong <owenl131@gmail.com>
2018-01-28 02:24:47 +0800

    Update server

--- backend/facialemotions.py
+++ backend/facialemotions.py
@@ -4,18 +4,19 @@ import base64
 import keys
 import http.client, urllib.request, urllib.parse, urllib.error
 import requests
+import json
 
-cascade_path = 'haarcascade_frontalface_default.xml'
+#cascade_path = 'haarcascade_frontalface_default.xml'
 cascade_path = 'haarcascade_frontalface_alt.xml'
 
 face_cascade = cv2.CascadeClassifier(cascade_path)
 
 
-def extract_face(img, output_size=(64, 64)):
+def extract_face(img, output_size=(128, 128)):
     faces = face_cascade.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
     if len(faces) == 0:
-        return img
-        # return cv2.resize(img, output_size)
+        #return img
+        return cv2.resize(img, output_size)
     x, y, w, h = faces[0]
     face = img[y:y+h, x:x+w]
     return cv2.resize(face, output_size)
@@ -32,17 +33,17 @@ def extract_emotion(img):
         cv2.imwrite('test.jpg', img)
         with open('test.jpg', 'rb') as f:
             bin_img = f.read()
-            #print(bin_img)
-            #bin_img = base64.encode(bin_img)
         url = keys.endpoint + '/emotion/v1.0/recognize'
         response = requests.post(url, headers=headers, data=bin_img)
-        print(response.content)
-        print(response.headers)
-        print(response.json)
-        print(response.reason)
-        print(response.status_code)
-        print(response.text)
+        x = response.json()
+        print(x)
+        x = x[0]['scores']
+        print(x)
+        for key in x:
+            x[key] = int(float(x[key])*100)
+        print(x)
     except Exception as e:
-        print('[Errno {0}] {1}'%(e.errno, e.strerror))
+        print(e)
     finally:
         os.remove('test.jpg')
+        return x
--- backend/server_facialemotion.py
+++ backend/server_facialemotion.py
@@ -3,14 +3,17 @@ import cv2
 import time
 import requests
 import threading
+import facialemotions as emo
 from threading import Thread, Event, ThreadError
 
 class Cam():
 
   def __init__(self, url):
     self.stream = requests.get(url, stream=True)
+    self.last_image = None
     self.thread_cancelled = False
     self.thread = Thread(target=self.run)
+    self.thread_img = Thread(target=self.process_image)
     print("camera initialised")
 
   def start(self):
@@ -21,7 +24,7 @@ class Cam():
     bytes = ''.encode()
     while not self.thread_cancelled:
       try:
-        bytes += self.stream.raw.read(1)
+        bytes += self.stream.raw.read(1024)
         b = bytes.rfind(b'\xff\xd9')
         a = bytes.rfind(b'\xff\xd8', 0, b-1)
         if a != -1 and b != -1 and a < b:
@@ -29,9 +32,22 @@ class Cam():
           bytes = bytes[b+2:]
           img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),
                              cv2.IMREAD_COLOR)
+          self.last_image = img
           cv2.imshow('cam', img)
           if cv2.waitKey(1) == 27:
             exit(0)
+      except ThreadError:
+        self.thread_cancelled = True
+
+  def process_image(self):
+    while not self.thread_cancelled:
+      try:
+        img = self.last_image.copy()
+        img = np.flip(img, axis=1)
+        img = np.swapaxes(img, 0, 1)
+        img_face = emo.extract_face(img)
+        self.last_emotion = emo.extract_emotion(img_face)
+        print(self.last_emotion)
       except ThreadError:
         self.thread_cancelled = True
 
